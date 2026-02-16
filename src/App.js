dc61035330dfa59e4debac7794bacae39ded9cc0
Abdul Rahman Saudagar <abdul.saudagar@affle.com>
2026-02-16 19:20:05 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>Dev : Sohail Saudagar changes</p>
+        <p>Dev : Sohail Saudagar</p>
       </header>
     </div>
   );
